dc538238f0211d38fa0af0b768d46838ef5f4f35
Manav Dutta <Manav.Dutta@capgroup.com>
2021-04-16 16:04:09 -0700

    add .gitignore to clean up repo

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import ReactDOM from 'react-dom';
-import App from './App';
-it('renders without crashing', function () {
-    var div = document.createElement('div');
-    ReactDOM.render(_jsx(App, {}, void 0), div);
-    ReactDOM.unmountComponentAtNode(div);
-});
